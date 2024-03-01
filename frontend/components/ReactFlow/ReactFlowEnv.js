e2e01ddf89bcd4dfdc5ba30828e775f714ee2448
zack <zack@zackbradshaw.com>
2024-03-01 17:42:27 -0600

    \:zap\: perf(ReactFlowEnv.js): optimize flow environment setup

--- frontend/components/ReactFlow/ReactFlowEnv.js
+++ frontend/components/ReactFlow/ReactFlowEnv.js
@@ -1,5 +1,5 @@
 import CustomNodeIframe from "../Nodes/Custom.js";
-import '../../css/dist/output.css'
+import '../../src/css/dist/output.css'
 import ReactFlow, { Background,
                     applyNodeChanges,
                     ReactFlowProvider,
@@ -13,7 +13,7 @@ import React ,{ useState, useCallback, useRef, useEffect } from 'react';
 import Navbar from '../Navagation/navbar';
 import CustomEdge from '../Edges/Custom'
 import CustomLine from "../Edges/CustomLine.js";
-import { useThemeDetector } from '../../helper/visual'
+import { useThemeDetector } from '../../src/helper/visual'
 import {CgMoreVerticalAlt} from 'react-icons/cg'
 import {BsFillEraserFill} from 'react-icons/bs' 
 import {FaRegSave} from 'react-icons/fa'
