21b4f3e4a32c8aaf39379655757cbdc7fa01e522
zack <zack@zackbradshaw.com>
2024-03-01 18:46:06 -0600

    perf: enhance _app.js loading speed :rocket:

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import '../src/css/index.css'
 import '../src/css/dist/output.css'; // Example of a global CSS file
+import '../src/css/counter.css';
 import 'semantic-ui-css/semantic.min.css'; // Assuming you're using Semantic UI
 
 // Your _app.js content;
