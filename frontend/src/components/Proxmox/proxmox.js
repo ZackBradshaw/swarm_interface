d7597b37249352d28f074151f56055f8b86c62d8
zack <zack@zackbradshaw.com>
2024-03-13 09:58:51 -0500

    🚀 feat: enhance Proxmox component with new socket logic

--- frontend/src/components/Proxmox/proxmox.js
+++ frontend/src/components/Proxmox/proxmox.js
@@ -1,24 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Terminal } from 'xterm';
-import 'xterm/css/xterm.css';
+import React, { useEffect, useState } from 'react';
 import socket from './socket';
 
 const ProxmoxVM = ({ vmAddress }) => {
-    const terminalRef = useRef(null);
-    const terminal = useRef(new Terminal());
     const [vncUrl, setVncUrl] = useState('');
 
     useEffect(() => {
-        terminal.current.open(terminalRef.current);
-        terminal.current.writeln('Connecting to VM...');
-
         // Emitting to fetch VM data as before
         socket.emit('fetchVmData', { vm_address: vmAddress });
-
-        // Listening for VM data to write to the terminal
-        socket.on('vmData', (data) => {
-            terminal.current.writeln(data);
-        });
 
         // Fetching VNC URL from your server
         // This assumes you have an endpoint that returns the noVNC URL for a given VM
@@ -30,13 +18,11 @@ const ProxmoxVM = ({ vmAddress }) => {
 
         return () => {
             socket.off('vmData');
-            terminal.current.dispose();
         };
     }, [vmAddress]);
 
     return (
         <div>
-            <div ref={terminalRef} />
             {vncUrl && (
                 <iframe
                     src={vncUrl}
